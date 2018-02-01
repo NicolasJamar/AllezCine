68a8b087554ba6b47fd50965f3d4c2395dc6a525
NicolasJamar <nicolas.jamar@gmail.com>
2018-02-01 17:04:43 +0100

    shop movies btn slide

--- assets/js/allezCine.js
+++ assets/js/allezCine.js
@@ -1,71 +1,15 @@
-// Function plus de films , moins de films tout ça ...
-$("#btnplusfilm").click(function(event) {
-  if ($('#btnplusfilm').html() == "Plus de Films") {
-    $('#btnplusfilm').html("Moins de Films");
-  } else {
-    $('#btnplusfilm').html("Plus de Films");
-  }
-});
-$("#btnplusserie").click(function(event) {
-  if ($('#btnplusserie').html() == "Plus de Séries") {
-    $('#btnplusserie').html("Moins de Séries");
-  } else {
-    $('#btnplusserie').html("Plus de Séries");
-  }
-});
 
-// Bouton Filtre
-var button = document.querySelector('#button-parent');
-var filterBtnArray = document.querySelectorAll('.btn_section');
-var btnArray = [];
-for (i=0;i<button.children.length;i++){
-  var filterBtn = document.querySelectorAll('.btn_section')[i];
-  var filterBtnContent = document.querySelectorAll('.btn_section')[i].innerHTML;
-  btnArray.push(filterBtnContent);}
 
-    $('.btn_section').click(function() {
-    if (btnArray[($(this).val())-1] === "All"){
-      if ( $('.vignettes').css("display") == "none"){
-        $('.vignettes').css("display","block");
-    }
-  }
-    else if (btnArray[($(this).val())-1] === "Thriller"){
-      $('.vignettes').css("display","block");
-      if ( $('.vignettes').not(".Thriller")){
-        $('.vignettes').not(".Thriller").css('display','none');
-      }
-    }
-    else if (btnArray[($(this).val())-1] === "Policiers"){
-      $('.vignettes').css("display","block");
-      if ( $('.vignettes').not(".Policiers")){
-        $('.vignettes').not(".Policiers").css('display','none');
-      }
-    }
-    else if (btnArray[($(this).val())-1] === "Comedie"){
-      $('.vignettes').css("display","block");
-      if ( $('.vignettes').not(".Comedie")){
-        $('.vignettes').not(".Comedie").css('display','none');
-      }
-    }
-    else if (btnArray[($(this).val())-1] === "Scifi"){
-      $('.vignettes').css("display","block");
-      if ( $('.vignettes').not(".Scifi")){
-        $('.vignettes').not(".Scifi").css('display','none');
-      }
-    }
-    else if (btnArray[($(this).val())-1] === "Dramatique"){
-      $('.vignettes').css("display","block");
-      if ( $('.vignettes').not(".Dramatique")){
-        $('.vignettes').not(".Dramatique").css('display','none');
-      }
-    }
-    else if (btnArray[($(this).val())-1] === "Aventure"){
-      $('.vignettes').css("display","block");
-      if ( $('.vignettes').not(".Aventure")){
-        $('.vignettes').not(".Aventure").css('display','none');
-      }
-    }
-    else {
-      $('.vignettes').css('display','block');
-    }
-    })
+
+
+var shopVideos = [
+"https://www.youtube.com/embed/JNwNXF9Y6kY", //Star Wars
+"https://www.youtube.com/embed/TElJs93LLs8", //Grave
+"https://www.youtube.com/embed/LEH7nDkiPEk", //Survive Style 5
+"https://www.youtube.com/embed/RMhbr2XQblk", //Gran Torino
+"https://www.youtube.com/embed/4TLppsfzQH8", //Limitless
+"https://www.youtube.com/embed/yfDUv3ZjH2k", //Shaun of the dead
+"https://www.youtube.com/embed/znmZoVkCjpI", //Seven
+"https://www.youtube.com/embed/hFY-f7BXwZw", //Swiss army man
+]
+
